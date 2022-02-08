1da2d2dffe56a7cd0db7211eb5a5ce5b28cc8c11
Miyuki <84234307+Miyukiinstant@users.noreply.github.com>
2022-02-08 11:45:14 +0100

    Intents DM

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 require('dotenv').config();
 const { Client, Intents, MessageEmbed } = require("discord.js");
 const { readdirSync } = require('fs');
-const client = new Client({intents: Intents.FLAGS.GUILDS});
+const client = new Client({intents: [Intents.FLAGS.GUILDS,
+                                    Intents.FLAGS.DIRECT_MESSAGES,
+                                Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+                            Intents.FLAGS.DIRECT_MESSAGE_TYPING]});
 const path = './commands/';
 client.on('ready',()=>{
     console.log(client.user.tag);
