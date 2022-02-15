d8f571cacab7fac9efe576726f3a5d5b57f21fb5
Miyuki <84234307+Miyukiinstant@users.noreply.github.com>
2022-02-15 12:44:02 +0100

    /Dumptruck

--- commands/DumpTruck.js
+++ commands/DumpTruck.js
@@ -3,17 +3,17 @@ const { MessageEmbed } = require("discord.js");
 module.exports= async client=>{
     client.on('interactionCreate',async interaction=>{
         if(!interaction.isCommand()) return;
-        if(interaction.commandName === 'weewee'){
+        if(interaction.commandName === 'butt'){
             const user = await interaction.user.fetch(true);
             interaction.reply({ embeds: [new MessageEmbed({
-                description: `<@${interaction.options._hoistedOptions[0].value}>'s weewee is ${Math.floor(Math.random()*30)}cm schlong :purple_heart:`,
+                description: `<@${interaction.options._hoistedOptions[0].value}>'s butt is ${Math.floor(Math.random()*30)} :watermelon: wide :purple_heart:`,
                 color: user.hexAccentColor
             })], ephemeral: false });
         }
     })
     return{
-        name:'weewee',
-        description:'Measure-o-weewee',
+        name:'butt',
+        description:'Measure-o-butt',
         options:[{
             name:'user',
             description:'pick user',
