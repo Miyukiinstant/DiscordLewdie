7fb25e31f407660da9becb194104f5a8bcfb19d1
Miyuki <84234307+Miyukiinstant@users.noreply.github.com>
2022-02-15 09:49:08 +0100

    fix

--- commands/Images.js
+++ commands/Images.js
@@ -1,3 +1,5 @@
+const { MessageAttachment, Message } = require('discord.js');
+const fs = require('fs')
 module.exports=async client=>{
     client.on('interactionCreate',async interaction=>{
         if(!interaction.isCommand())return;
@@ -5,20 +7,23 @@ module.exports=async client=>{
             var json_images = {
                 urls:[]
             }
-            var urls = ""
             const channelID = interaction.options._hoistedOptions[0].value;
             const channels = interaction.guild.channels.cache
             channels.map(async (index)=>{
                 if (`${index.id}` === `${channelID}`) { //935251511234138112
                     const res = await index.messages.fetch();
-                        res.map((index)=>{
-                            index.attachments.map(async (value)=>{
-                                json_images.urls.push(JSON.stringify(value.attachment,null,'\t'))
-                            })                        
-                        }) 
-                         
-                    }
-                    console.log(json_images);
+                    res.map((index)=>{
+                        index.attachments.map(async (value)=>{
+                            json_images.urls.push(JSON.stringify(value.attachment))
+                        })                        
+                    })          
+                    var filename = `${Date.now()}.json`;
+                    fs.writeFileSync(filename,JSON.stringify(json_images,null,'\t'),'utf-8')
+                    interaction.channel.send({
+                        files:[filename]
+                    })
+                    fs.unlinkSync(filename);
+                }
             })                        
         }
     })
