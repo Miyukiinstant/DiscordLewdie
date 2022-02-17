7284ab1e5658a5deeadddea2ca4a8f8b97d9bf07
Miyuki <84234307+Miyukiinstant@users.noreply.github.com>
2022-02-17 11:35:32 +0100

    -

--- commands/Petpet.js
+++ commands/Petpet.js
@@ -6,16 +6,19 @@ module.exports = async client=>{
         if(!interaction.isCommand()) return;
         if(interaction.commandName === 'petpet'){
             const user = interaction.options._hoistedOptions[0].member;
+            const g_user = await user.user.fetch(true)
             const animatedGif = await petPetGif(user.displayAvatarURL({dynamic:true,format:'png'}),{
-                resolution: 256
+                resolution: 256, // The width (or height) of the generated gif
             })
             interaction.reply({ embeds: [new MessageEmbed({
+                description: `Petpet <@${user.id}>`,
+                color: g_user.hexAccentColor
             })],files:[
                 {
                 attachment:animatedGif,
                 name:'petPetGif.gif'
             }
-        ], ephemeral: false });
+        ], ephemeral: true });
         }
     })
     return{
