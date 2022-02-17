10474f3d8f471776c532024b112662fab1a93a05
Miyuki <84234307+Miyukiinstant@users.noreply.github.com>
2022-02-17 11:35:47 +0100

    reduced res

--- commands/Petpet.js
+++ commands/Petpet.js
@@ -8,7 +8,7 @@ module.exports = async client=>{
             const user = interaction.options._hoistedOptions[0].member;
             const g_user = await user.user.fetch(true)
             const animatedGif = await petPetGif(user.displayAvatarURL({dynamic:true,format:'png'}),{
-                resolution: 256, // The width (or height) of the generated gif
+                resolution: 128, // The width (or height) of the generated gif
             })
             interaction.reply({ embeds: [new MessageEmbed({
                 description: `Petpet <@${user.id}>`,
