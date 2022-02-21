601737a07f9de7199df076317f88fb43824d40d1
Miyukiinstant <miyuki.ram01@gmail.com>
2022-02-21 12:45:33 +0100

    added hex color

--- commands/Inspirobot.js
+++ commands/Inspirobot.js
@@ -5,8 +5,11 @@ module.exports = async client=>{
     client.on('interactionCreate',async interaction=>{
         if(!interaction.isCommand()) return;
         if(interaction.commandName === 'inspiro'){
+            const user = interaction.options._hoistedOptions[0].member
+            const g_user = await user.user.fetch(true)
             const inspiro = await axios.get('https://inspirobot.me/api?generate=true')
             interaction.reply({ embeds: [new MessageEmbed({
+                color: `${g_user.hexAccentColor}`,
                 image:{
                     url: inspiro.data
                 },
